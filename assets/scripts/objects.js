cefeb7c49035383f5a459090c1f330d7b695c973
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-10-03 15:26:14 -0400

    rendering movies

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,31 +1,49 @@
-const addMovieBtn = document.getElementById('add-movie-btn');
-const searchBtn = document.getElementById('search-btn');
+const addMovieBtn = document.getElementById("add-movie-btn");
+const searchBtn = document.getElementById("search-btn");
 
 const movies = [];
 
-const addMovieHandler = () => {
-  const title = document.getElementById('title').value;
-  const extraName = document.getElementById('extra-name').value;
-  const extraValue = document.getElementById('extra-value').value;
+const renderMovies = () => {
+	const movieList = document.getElementById("movie-list");
 
-  if (
-    title.trim() === '' ||
-    extraName.trim() === '' ||
-    extraValue.trim() === ''
-  ) {
-    return;
-  }
+	if (movies.length === 0) {
+		movieList.classList.remove("visible");
+		return;
+	} else {
+		movieList.classList.add("visible");
+	}
+	movieList.innerHTML = "";
 
-  const newMovie = {
-    info: {
-      title,
-      [extraName]: extraValue
-    },
-    id: Math.random()
-  };
-
-  movies.push(newMovie);
-  console.log(newMovie);
+	movies.forEach((movie) => {
+		const movieEl = document.createElement("li");
+		movieEl.textContent = movie.info.title;
+		movieList.append(movieEl);
+	});
 };
 
-addMovieBtn.addEventListener('click', addMovieHandler);
+const addMovieHandler = () => {
+	const title = document.getElementById("title").value;
+	const extraName = document.getElementById("extra-name").value;
+	const extraValue = document.getElementById("extra-value").value;
+
+	if (
+		title.trim() === "" ||
+		extraName.trim() === "" ||
+		extraValue.trim() === ""
+	) {
+		return;
+	}
+
+	const newMovie = {
+		info: {
+			title,
+			[extraName]: extraValue,
+		},
+		id: Math.random(),
+	};
+
+	movies.push(newMovie);
+	renderMovies();
+};
+
+addMovieBtn.addEventListener("click", addMovieHandler);
